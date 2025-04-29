c980b4e18ce9b8cb58c099ddd93f4ef4557dc2d1
Sameer Paudel <paudelsameer888@gmail.com>
2025-04-29 18:23:07 +0545

    never issue: Return Types fixed

--- returnTypes.ts
+++ returnTypes.ts
@@ -7,11 +7,23 @@ const hello = (name: string): string => {
 }
 
 //void: for functions that don't return anything
-function sayMyname():void{
+function sayMyname(): void {
     console.log('Heisenberg');
+
+}
+// //Never: Used for functions that never return anything
+// //Some functions like the ones who only throw error or which only have an infinite loop. They never return. So for them never is used. A function with return never MUST not have a reachable point
+const ThrowError = (msg: string): never => {
+    throw new Error(msg)
     
 }
-//Never: 
+
+ThrowError('Noooo')
+
+const infiniteloop = ():never => {
+    while(true) { }
+}
+
 
 console.log(addUs(5, 6));
 console.log(hello('shreeya'))
